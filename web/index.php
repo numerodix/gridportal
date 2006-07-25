b4880cd7892c67ac4add41d7e376a558e96a5ab5
numerodix <numerodix@gmail.com>
2006-07-25 07:54:07 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/grid-portal/code@12 0a074c2d-d918-0410-9169-c494ef5cfd04

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+        <title>GRIDportal project</title>
+
+<link rel="stylesheet" href="/styles.css" type="text/css">
+<link rel="stylesheet" href="gridportal/_scripts/styles.css" type="text/css">
+</head>
+
+<body>
+
+<?      include ("menu.php");        ?>
+<br>
+<center><a href="gridportal"><img src="logo.png" border=0></a></center>
+
+</body>
+</html>
